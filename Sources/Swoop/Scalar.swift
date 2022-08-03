473fd1d3494b6b73950ffd7622fbab9d2b318825
Jesse Seidman <seidmanjesse@gmail.com>
2022-08-02 22:55:10 -0400

    took out the equatable because it wasn't needed

--- Sources/Swoop/Scalar.swift
+++ Sources/Swoop/Scalar.swift
@@ -7,28 +7,8 @@
 
 import Foundation
 
-public protocol Scalar<Value>: SwoopEquatable {
+public protocol Scalar<Value> {
   associatedtype Value
 
   func value() throws -> Value
-}
-
-public protocol SwoopEquatable {
-  associatedtype Value
-
-  func equals(_ obj: Value) throws -> Bool
-}
-
-public extension Scalar where Value: Equatable {
-
-  func equals(_ obj: Value) throws -> Bool {
-    do {
-      return try value() == obj
-    } catch let e as Exception {
-      throw StandardException(
-        "Could not equate self with value \(obj)",
-        wrapping: e
-      )
-    }
-  }
 }
